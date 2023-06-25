bb1d3a46010ce96207f546b05026f6fc8f770b16
solo <soloman.err@.com>
2023-06-26 03:28:56 +0600

    firebase configuration modified with alphabetor

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,14 +1,14 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app';
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  appId: import.meta.env.VITE_APPID,
 }
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig)
 
-export default firebaseApp
+export default firebaseApp;
